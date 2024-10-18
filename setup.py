63d9635238e5548720be28c733bcedcfb16eb0ca
Raul Mauricio Uñate Castro <runate@ccl.com.co>
2024-10-18 18:30:32 -0500

    Modified files: M .gitignore  M setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="flaskavel",
-    version="0.0.7",
+    version="0.0.8",
     author="Raul Mauricio Uñate Castro",
     author_email="raulmauriciounate@gmail.com",
     description="Like in Laravel but with Python",
