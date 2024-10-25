bfc2c3fe21aee20482dbf49170139633918ae9d4
Raul Mauricio Uñate Castro <runate@ccl.com.co>
2024-10-25 10:18:30 -0500

    lab.beaker.console.commands.make_controller  lab.beaker.console.stub.Controller.stub  metadata

--- flaskavel/lab/beaker/console/commands/make_controller.py
+++ flaskavel/lab/beaker/console/commands/make_controller.py
@@ -1,4 +1,5 @@
 import os
+import re
 from pathlib import Path
 from flaskavel.lab.beaker.console.reactor import reactor
 from flaskavel.lab.beaker.console.command import Command
@@ -18,17 +19,20 @@ class MakeController(Command):
         Defines the command-line arguments for the 'make:controller' command.
         """
         return [
-            ('--name', {'type': str, 'required': True, 'default': True, 'help': 'Create a controller into "Http/Controllers" folder.'})
+            ('--name', {'type': str, 'required': True, 'default': True, 'help': 'Create a controller into "app/Http/Controllers/" folder.'})
         ]
 
     def handle(self) -> None:
+
         """
         Handles the execution of the 'make:controller' command to create a controller.
         """
+
         try:
+
             # Retrieve the argument
             name: str = self.argument('name')
-            controllers_base_path = os.path.join(app_path(), 'Http/Controllers')
+            controllers_base_path = app_path('Http/Controllers')
 
             # Separate route and filename
             if '/' in name:
@@ -39,17 +43,24 @@ class MakeController(Command):
                 # If no subfolders, assign base path
                 sub_path = controllers_base_path
                 controller_name = name
-            
+
             # Clean spaces only in the controller file name
             controller_name = controller_name.replace(" ", "")
-            
+
+            # Regex pattern that allows only alphabetic characters and underscores
+            pattern = r'^[a-zA-Z_]+$'
+
+            # Validate controller name against the pattern
+            if not re.match(pattern, controller_name):
+                raise ValueError("Controller name must only contain alphabetic characters and underscores (_), no numbers or special characters are allowed.")
+
             # Create the subdirectory if it doesn't exist
             os.makedirs(sub_path, exist_ok=True)
 
             # Verify if the name file already exists
             controller_filename = f"{controller_name}.py"
             existing_files = [f.lower() for f in os.listdir(sub_path) if os.path.isfile(os.path.join(sub_path, f))]
-            
+
             if controller_filename.lower() in existing_files:
                 raise ValueError(f"A controller with the name '{controller_name}' already exists in the directory: {sub_path}")
 
@@ -60,13 +71,13 @@ class MakeController(Command):
 
             # Replace {{name-Controller}} with the controller name
             controller_content = template_content.replace('{{name-Controller}}', controller_name)
-            
+
             # Create and save the new controller file
             new_controller_path = os.path.join(sub_path, controller_filename)
             with open(new_controller_path, 'w') as new_file:
                 new_file.write(controller_content)
 
-            print(f"Controller '{controller_name}' created successfully in {sub_path}")
+            self.info(f"Controller '{controller_name}' created successfully in {sub_path}")
 
         except ValueError as e:
             raise ValueError(f"Error: {e}")
--- flaskavel/metadata.py
+++ flaskavel/metadata.py
@@ -1,5 +1,5 @@
 NAME="flaskavel"
-VERSION="1.0.0"
+VERSION="1.1.0"
 AUTHOR="Raul Mauricio Uñate Castro"
 AUTHOR_EMAIL="raulmauriciounate@gmail.com"
 DESCRIPTION="Like in Laravel but with Python"
