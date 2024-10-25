5ed25636bc1270b29cd83c55e4bc82706adc2ba9
Raul Mauricio Uñate Castro <runate@ccl.com.co>
2024-10-25 10:28:04 -0500

    metadata  setup

--- setup.py
+++ setup.py
@@ -11,6 +11,7 @@ setup(
     long_description_content_type='text/markdown',
     url=FRAMEWORK,
     packages=find_packages(),
+    include_package_data=True,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
