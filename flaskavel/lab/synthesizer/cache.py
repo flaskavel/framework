8426e1e10e94d644e562719d31fe28b961daf4f2
Raul Mauricio Uñate Castro <runate@ccl.com.co>
2024-10-25 11:36:02 -0500

    lab.synthesizer.cache  metadata

--- flaskavel/lab/synthesizer/cache.py
+++ flaskavel/lab/synthesizer/cache.py
@@ -16,7 +16,7 @@ class FlaskavelCache:
             basePath (Path): The base path for the application.
         """
         self.basePath = basePath
-        self.root_project = str(basePath).replace(os.sep,'_').lower()
+        self.root_project = str(os.path.dirname(__file__)).replace(os.sep, '_').lower()
         self.started_file = self.root_project + 'started.lab'
 
     def clearStart(self):
--- flaskavel/metadata.py
+++ flaskavel/metadata.py
@@ -1,5 +1,5 @@
 NAME="flaskavel"
-VERSION="1.4.0"
+VERSION="1.4.1"
 AUTHOR="Raul Mauricio Uñate Castro"
 AUTHOR_EMAIL="raulmauriciounate@gmail.com"
 DESCRIPTION="Like in Laravel but with Python"
