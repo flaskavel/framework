e2ef31a9d51731c5c40cd78cdaaaebcdb0064ca7
Raul Mauricio Uñate Castro <runate@ccl.com.co>
2025-01-30 14:53:39 -0500

    📦 Release version 1.11.9

--- flaskavel/luminate/installer/upgrade.py
+++ flaskavel/luminate/installer/upgrade.py
@@ -1,13 +1,34 @@
-from flaskavel.luminate.contracts.installer.upgrade_interface import IUpgrade
 import subprocess
 import sys
 
-class Upgrade(IUpgrade):
+class Upgrade:
+    """
+    A class responsible for handling the upgrade process of Flaskavel.
+
+    Methods
+    -------
+    execute() : None
+        Executes the upgrade process to install the latest version of Flaskavel.
+    """
 
     @staticmethod
     def execute():
         """
         Handle the --upgrade command to update Flaskavel to the latest version.
+
+        This method attempts to upgrade Flaskavel using the pip package manager.
+        It executes a command that installs the latest version of Flaskavel, ensuring
+        the application is up-to-date.
+
+        Raises
+        ------
+        ValueError
+            If the upgrade process fails or encounters any error during execution.
+
+        Notes
+        -----
+        The upgrade process uses `pip` via the command:
+        `python -m pip install --upgrade flaskavel`.
         """
         try:
             subprocess.check_call([sys.executable, "-m", "pip", "install", "--upgrade", "flaskavel"])
