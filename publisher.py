059a8964f61128e7e27442596c11b6928c514ef5
Raul Mauricio Uñate Castro <runate@ccl.com.co>
2025-01-31 14:14:32 -0500

    📦 Release version 1.11.13

--- publisher.py
+++ publisher.py
@@ -19,7 +19,6 @@ if __name__ == "__main__":
     """
     try:
         publisher = PypiPublisher()
-        publisher.clearRepository()
         publisher.gitPush()
         publisher.publish()
         publisher.clearRepository()
