1bf69f2c0f0ce95e5227baa1a334c9e2400b32d7
Raul Mauricio Uñate Castro <runate@ccl.com.co>
2025-01-31 21:14:08 -0500

    📦 Release version 1.11.23

--- flaskavel/luminate/publisher/pypi.py
+++ flaskavel/luminate/publisher/pypi.py
@@ -49,7 +49,6 @@ class PypiPublisher(IPypiPublisher):
         project_root : str
             The root directory of the project where the process is executed.
         """
-        Console.clear()
         self.token = token or os.getenv("PYPI_TOKEN").strip()
         self.python_path = sys.executable
         self.project_root = os.getcwd()
