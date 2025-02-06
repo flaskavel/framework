5142318ea3f3d6be9e9459a661b472ab3972fa57
Raul Mauricio Uñate Castro <runate@ccl.com.co>
2025-02-05 19:17:42 -0500

    📦 Release version 1.27.0

--- flaskavel/luminate/pipelines/CLIPipeline.py
+++ flaskavel/luminate/pipelines/CLIPipeline.py
@@ -2,8 +2,11 @@ class CLIPipeline:
     pass
 
     # def __init_():
-
     #     1. Cargar Comandos a Cache
-    #     2. Cargar Rutas
-    #     2. Cargar Middlewares
-    #     3. Obtener nombre de comando
+    #     2. Cargar Rutas (pass)
+    #     3. Cargar Middlewares (pass)
+    #     4. Obtener Variables (arguments)
+    #     5. Ejecutar Middleware (pass)
+    #     6. Ejecutar Comando
+    #     7. Ejecutar after.
+    #     8. finalizar proceso (Limpiar Hilo)
