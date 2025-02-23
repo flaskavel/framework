169291db78cf468e7eb611ccd3a2f05cc469d684
Raul Mauricio Uñate Castro <runate@ccl.com.co>
2025-02-23 14:31:26 -0500

    📦 Release version 0.70.0

--- orionis/framework.py
+++ orionis/framework.py
@@ -5,7 +5,7 @@
 NAME = "orionis"
 
 # Current version of the framework
-VERSION = "0.68.0"
+VERSION = "0.70.0"
 
 # Full name of the author or maintainer of the project
 AUTHOR = "Raul Mauricio Uñate Castro"
--- orionis/luminate/services/environment/environment_service.py
+++ orionis/luminate/services/environment/environment_service.py
@@ -1,5 +1,7 @@
+import ast
 import os
 from pathlib import Path
+from typing import Any
 from dotenv import set_key, unset_key, dotenv_values
 from orionis.contracts.services.environment.i_environment_service import IEnvironmentService
 
@@ -51,11 +53,16 @@ class EnvironmentService(IEnvironmentService):
         str
             The value of the environment variable or the default value.
         """
+
         # Get the value from the .env file
         value = dotenv_values(self.path).get(key)
 
-        # Return the value or the default value
-        return value if value is not None else os.getenv(key, default)
+        # Get the value from the system environment variables if not found
+        if value is None:
+            value = os.getenv(key)
+
+        # Parse the value and return it
+        return self._parse_value(value) if value is not None else default
 
     def set(self, key: str, value: str) -> None:
         """
@@ -93,4 +100,32 @@ class EnvironmentService(IEnvironmentService):
             A dictionary of all environment variables and their values.
         """
         # Return all environment variables
-        return dotenv_values(self.path)
+        env_vars = {}
+
+        # Get all environment variables from the .env file
+        data =  dotenv_values(self.path)
+        for key, value in data.items():
+            # Parse the value and add it to the dictionary
+            env_vars[key] = self._parse_value(value)
+
+        # Get all environment variables from the system environment variables
+        return env_vars
+
+    def _parse_value(self, value : Any):
+
+        # Strip leading and trailing whitespace from the value
+        value = str(value).strip() if value is not None else None
+
+        # Parse common types and Python literals
+        if not value or value.lower() in {'none', 'null'}:
+            return None
+        if value.lower() in {'true', 'false'}:
+            return value.lower() == 'true'
+        if value.isdigit():
+            return int(value)
+
+        # Attempt to parse Python literals (e.g., lists, dictionaries)
+        try:
+            return ast.literal_eval(value)
+        except (ValueError, SyntaxError):
+            return value
